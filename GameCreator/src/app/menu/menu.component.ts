06766152fe77f993819a54d6975852194704e5e0
Laura <laura.binder@edu.campus02.at>
2022-01-27 21:10:56 +0100

    change icon

--- GameCreator/src/app/menu/menu.component.ts
+++ GameCreator/src/app/menu/menu.component.ts
@@ -1,6 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-
 
 @Component({
   selector: 'app-menu',
@@ -9,20 +7,8 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 })
 export class MenuComponent implements OnInit {
 
-  public FirstFormGroup: FormGroup;
-  public SecondFormGroup: FormGroup;
-
-  isEditable = false;
-
-  constructor(private _formBuilder: FormBuilder) {}
-
   ngOnInit() {
-    this.FirstFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required],
-    });
-    this.SecondFormGroup = this._formBuilder.group({
-      secondCtrl: ['', Validators.required],
-    });
+
   }
 
 }
