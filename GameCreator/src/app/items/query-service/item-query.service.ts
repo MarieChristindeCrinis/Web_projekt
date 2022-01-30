0c411ede8a67f46afc46fa5ab211fa890c8c893f
Laura Binder <laura.binder@edu.campus02.at>
2022-01-30 12:02:46 +0100

    fix item view

--- GameCreator/src/app/items/query-service/item-query.service.ts
+++ GameCreator/src/app/items/query-service/item-query.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IItemModel } from '../model/IItemModel';
 import { HttpClient } from '@angular/common/http';
+import { ServerLocationService } from 'src/app/shared/server-location/server-location.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,9 @@ export class ItemQueryService {
 
   public AvailableItems: Observable<IItemModel[]>;
 
-  constructor(httpService: HttpClient)
+  constructor(serverLocationProvider: ServerLocationService, httpService: HttpClient)
   {
-    this.AvailableItems = httpService.get<IItemModel[]>('items');
+    this.AvailableItems = httpService.get<IItemModel[]>(serverLocationProvider.ServerLocation + 'items');
   }
 
 }
